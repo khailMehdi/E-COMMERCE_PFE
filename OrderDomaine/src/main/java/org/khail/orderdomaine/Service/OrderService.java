b489244f2c5f1e68060018047a0dc0a2f792eea3
khailMehdi <154886661+khailMehdi@users.noreply.github.com>
2025-05-16 14:57:49 +0100

    Update OrderService.java

--- OrderDomaine/src/main/java/org/khail/orderdomaine/Service/OrderService.java
+++ OrderDomaine/src/main/java/org/khail/orderdomaine/Service/OrderService.java
@@ -7,7 +7,8 @@ import java.util.List;
 public interface OrderService {
     public List<Order> getAllOrders();
     public Order getOrderById(int id);
-    public Order addOrder(Order order);
+   // public Order addOrder(Order order);
+    public Order createOrder(Order order);
     public void deleteOrder(int id);
 
 

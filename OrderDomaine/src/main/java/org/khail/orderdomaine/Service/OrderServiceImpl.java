411637adb2bdc219651b0164c79de8e1dcd61ac7
khailMehdi <154886661+khailMehdi@users.noreply.github.com>
2025-05-16 14:58:46 +0100

    Update OrderServiceImpl.java

--- OrderDomaine/src/main/java/org/khail/orderdomaine/Service/OrderServiceImpl.java
+++ OrderDomaine/src/main/java/org/khail/orderdomaine/Service/OrderServiceImpl.java
@@ -18,10 +18,10 @@ public class OrderServiceImpl implements OrderService {
         return orderRepository.findAll();
     }
 
-    @Override
+   /* @Override
     public Order getOrderById(int id) {
         return orderRepository.findById(id).orElse(null);
-    }
+    } */
 
     @Override
     public Order addOrder(Order order) {
@@ -31,5 +31,25 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public void deleteOrder(int id) {
       orderRepository.deleteById(id);
+    }
+     @Override
+    public Order createOrder(Order order) {
+        // Vérifie que le client existe
+        Customer customer = customerServiceClient.getCustomersById(order.getCustomerId());
+        System.out.println(customer);
+        // 2. Calculer le totalAmount à partir des lignes de commande (optionnel mais recommandé)
+        BigDecimal totalAmount = order.getOrderLines().stream()
+                .map(line -> line.getOrder().getTotalAmount().multiply(BigDecimal.valueOf(line.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        order.setTotalAmount(totalAmount);
+
+        // 3. Lier les lignes d'order à l'ordre parent
+        order.getOrderLines().forEach(line -> line.setOrder(order));
+
+        // 4. Définir la date de création si besoin (souvent géré par @CreatedDate)
+        if (order.getCreatedDate() == null) {
+            order.setCreatedDate(LocalDateTime.now());
+        }
+        return orderRepository.save(order);
     }
 }

bbe687b6a6f7a292e5033015aa97eac9e98eef5b
khailMehdi <154886661+khailMehdi@users.noreply.github.com>
2025-05-16 14:53:50 +0100

    Update DiscoveryServiceApplication.java

--- DiscoveryService/src/main/java/org/khail/discoveryservice/DiscoveryServiceApplication.java
+++ DiscoveryService/src/main/java/org/khail/discoveryservice/DiscoveryServiceApplication.java
@@ -2,11 +2,11 @@ package org.khail.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServiceApplication.class, args);
